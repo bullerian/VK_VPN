1b4d12b9e8b802fed24824d21889d7bcfefe6f4e
Dmytro <lizometrix@gmail.com>
2017-05-21 12:43:08 +0300

    Script fixed.Use this as starting point

--- install.sh
+++ install.sh
@@ -3,26 +3,17 @@
 #Description: VK_VPN service install script
 #Dependencies: openvpn, opera
 
-
-
 #User have to be logged as root
-# if [ $(id -u) -gt 0 ]; then
-#   echo "You have to be logged as root to proceed with installation. Terminating."
-# #  exit 1;
-# fi
+if [ $(id -u) -gt 0 ]; then
+  echo "You have to be logged as root to proceed with installation. Terminating."
+  exit 1;
+fi
 
+#Check if dependecy is installed.
+#Returns 0 if package is instaled or greater than zero if not.
 isDependecyPresent() {
   local nameOfDepend=$1
 
   dpkg -s $nameOfDepend 2>/dev/null 1>/dev/null;
   echo $?;
 }
-
-#Check if openvpn is allready installed
-
-
-
-
-var=$(isDependecyPresent 'vim');
-
-echo $var;
