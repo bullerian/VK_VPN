9341f7856f2c6c085bd336fd9c5c79846cfd35f4
Dmytro <lizometrix@gmail.com>
2017-05-24 13:31:10 +0300

    OpenVPN is set up to route only vk.com traffic; *.ovpn file autodownloads; systemd service added

--- install.sh
+++ install.sh
@@ -2,13 +2,14 @@
 
 #Description: VK_VPN service install script
 #Dependencies: openvpn, opera
-declare -a dependencies=( 'openvpn' 'opera' )
+declare -a dependencies=( 'openvpn' 'opera-stable' )
 
-#User have to be logged as root
-if [ $(id -u) -gt 0 ]; then
-  echo "You have to be logged as root to proceed with installation. Terminating."
-  exit 1;
-fi
+#.ovpn file url
+pathToOvpnFile="http://www.vpngate.net/common/openvpn_download.aspx?sid=149\
+5615054528&udp=1&host=vpn203976800.opengw.net&port=1933&hid=3388218&/vpngate_vpn\
+203976800.opengw.net_udp_1933.ovpn"
+
+vkRoutes='1i#Route only vk.com traffic\nroute-nopull\nroute 87.240.165.82\nroute 95.213.11.180\n'
 
 #Check if dependecy is installed.
 #Returns 0 if package is instaled or greater than zero if not.
@@ -38,11 +39,45 @@ Terminating."
   fi
 }
 
+#Add Opera deb repo, update packages list
+addOperaRepo(){
+  echo "Adding Opera repository"
+  add-apt-repository 'deb https://deb.opera.com/opera-stable/ stable non-free'
+  wget -qO- https://deb.opera.com/archive.key | apt-key add -
+  apt-get update
+}
+
 #Greeting message
 echo -e "VK_VPN service installer script\n"
 
+#Check if user is logged as root. Exit if not.
+if [ $(id -u) -gt 0 ]; then
+  echo "You have to have root privileges to proceed with installation. Terminating."
+  exit 1;
+fi
+
 #List dependencies
 echo "Trying to resolve VK_VPN service dependencies:"
+#Call function to add Opera deb repo to repos list
+addOperaRepo
+
+#Check and install all packages from the "dependencies" array
 for pkg in ${dependencies[*]}; do
   installPackage "$pkg"
 done
+
+#Download .ovpn config file from vpngate.net and add vk routes to beginning of
+#openVPN config file
+wget -qO config.conf $pathToOvpnFile && sed -i "$vkRoutes" config.conf
+
+#copy files to their locations
+if [ ! -d "$HOME/.config/systemd/user" ]; then
+  mkdir -p "$HOME/.config/systemd/user"
+fi
+
+cp config.conf /etc/openvpn && cp vk_vpn.service $HOME/.config/systemd/user && \
+sed -i 's/#AUTOSTART="all"\(.*#AUTOSTART="none"\)/AUTOSTART="config"\1/'\
+ /etc/default/openvpn
+
+#Service section
+systemctl --user enable vk_vpn.service
